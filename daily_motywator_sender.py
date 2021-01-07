8722c73af977c76273c2ea7ffccf1dc3bbbe1413
Kozikowski Marcin <marcin.kozikowski@gfieast.com>
2021-01-07 15:15:00 +0100

    add caption to photos

--- daily_motywator_sender.py
+++ daily_motywator_sender.py
@@ -5,12 +5,11 @@ import pytz
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
-telegram_bot_token = "#token#"
+telegram_bot_token = "#token"
+directory_in_str = '#path/#'
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
-directory_in_str = '#photos_directory#'
-posted_directory_in_str = '#photos_posted#'
 daily_flag = 1
 
 def daily_job(update, context):
@@ -20,17 +19,22 @@ def daily_job(update, context):
         daily_flag = 0
         chat_id = update.effective_chat.id
         context.bot.send_message(chat_id=chat_id, text='Setting a daily motywator!')
-        # context.job_queue.run_repeating(send_photo, interval=30, first=0, context=update.message.chat_id)
         t = datetime.time(hour=12, minute=45, tzinfo=pytz.timezone('Europe/Warsaw'))
         context.job_queue.run_daily(send_photo, t, days=tuple(range(5)), context=update.message.chat_id)
+
 
 def send_photo(context):
     chat_id = context.job.context
     fnames = os.listdir(directory_in_str)
-    filename = fnames[0]     
-    filepath = directory_in_str + filename  
-    context.bot.send_photo(chat_id = chat_id, photo=open(filepath, 'rb'))
-    os.rename(filepath, posted_directory_in_str + filename)
+    fjpeg = list(filter(lambda k: 'jpg' in k, fnames))
+    photo_name = fjpeg[0]
+    caption_file_path = directory_in_str + os.path.splitext(photo_name)[0] + '.txt'
+    caption_string = open(caption_file_path, 'r').read()
+    photo_path = directory_in_str + photo_name
+    context.bot.send_photo(chat_id=chat_id, photo=open(photo_path, 'rb'), caption=caption_string)
+    os.remove(photo_path)
+    os.remove(caption_file_path)
+
 
 dispatcher.add_handler(CommandHandler("daily", daily_job, pass_job_queue=True))
 updater.start_polling()
